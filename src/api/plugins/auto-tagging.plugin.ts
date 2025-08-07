d2bc6ef8470a9401ebdff5ef473d90217b32fc02
DenysPoliarush <66991536+DenysPoliarush@users.noreply.github.com>
2025-08-07 16:47:04 +0300

    cleanup

--- src/api/plugins/auto-tagging.plugin.ts
+++ src/api/plugins/auto-tagging.plugin.ts
@@ -5,22 +5,22 @@ const plugin: FastifyPluginAsync = async function (fastify) {
   fastify.addHook('onRoute', (routeOptions) => {
     if (routeOptions.url) {
       // First try to match `/api/admin/{tag}/...`
-      let pathMatch = routeOptions.url.match(/^\/api\/admin\/([^\/]+)/);
+      let pathMatch = routeOptions.url.match(/^\/api\/admin\/([^/]+)/);
       // If no match is found, try to match `/api/{tag}/...`
       if (!pathMatch) {
-        pathMatch = routeOptions.url.match(/^\/api\/([^\/]+)/);
+        pathMatch = routeOptions.url.match(/^\/api\/([^/]+)/);
       }
       // Use the first capturing group as the tag, or 'default' if no matches are found
       const tag = pathMatch ? pathMatch[1] : 'default';
 
       // Add or modify the existing tags
       if (!routeOptions.schema) {
-        // eslint-disable-next-line no-param-reassign
+         
         routeOptions.schema = {};
       }
 
       const existingTags = routeOptions.schema.tags || [];
-      // eslint-disable-next-line no-param-reassign
+       
       routeOptions.schema.tags = [...existingTags, tag];
     }
   });
